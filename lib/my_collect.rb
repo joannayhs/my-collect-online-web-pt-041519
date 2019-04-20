addb0d14a881ca11be09cda41374c81a75cf4b2b
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-20 19:50:25 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,7 @@ def my_collect(array)
   counter = 0 
   new_array = []
   while counter > array.length
-   yield(array[counter])
+   new_array << yield(array[counter])
    counter += 1 
  end
  new_array
